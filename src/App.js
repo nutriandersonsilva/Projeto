9877f84a92e32e2d51f70a09f3a2d554d4cdb075
Anderson Silva <nutriandersonsilva@gmail.com>
2022-11-23 18:49:10 -0300

    todas as atividades

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import Content from './Content';
+import Footer from './Footer';
+import Header from './Header';
 
 function App() {
   return (
-    <h1>Exercise - Frontend content</h1>
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+
   );
 }
 
